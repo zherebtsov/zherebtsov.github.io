6070ffee594b7c98077a9bd280e0b88de3894604
Dmitry <dmitryje@ya.ru>
2018-11-26 00:16:24 +0300

    add project constructor form

---
+++ projects/constructor-form/precache-manifest.0a55812f2b5a0fd4a51cf61057e6247e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1fc9d6fb64500bb3ef8b",
+    "url": "./static/js/main.1fc9d6fb.chunk.js"
+  },
+  {
+    "revision": "7a973834514b7d84e0e1",
+    "url": "./static/js/1.7a973834.chunk.js"
+  },
+  {
+    "revision": "aed09d18f1f8c7506bbe4d0312eee847",
+    "url": "./index.html"
+  }
+];
